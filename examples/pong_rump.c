3d99508a8ff3f9fbf9fe045b8d52fcafd23074ea
Martin Unzner <abmelden22@gmx.de>
2014-02-09 14:35:00 +0100

    First try on localhost comm., did not work

--- examples/pong_rump.c
+++ examples/pong_rump.c
@@ -109,8 +109,10 @@ int main(int argc, char *argv[]) {
 
     ERR("Setting IP address %s\n", ip_address_str);
     rump_pub_netconfig_ipv4_ifaddr(
-            "shmif0", ip_address_str, "255.255.255.0");
-    rump_pub_netconfig_ifup("shmif0");
+            "shmif0", ip_address_str, "255.255.255.255");
+    if (rump_pub_netconfig_ifup("shmif0") < 0) {
+        die(errno, "ipup");
+    }
 
     ERR("Creating Socket\n");
     tcpsock = rump_sys_socket(PF_INET, SOCK_STREAM, 0);
@@ -118,7 +120,7 @@ int main(int argc, char *argv[]) {
         die(errno, "socket");
     }
 
-    char const *srv_address = "10.93.49.2";
+    char const *srv_address = "10.93.49.100";
     long req_port = 26420;
     if (argc > 1) {
         req_port = strtol(argv[1], NULL, 0);
